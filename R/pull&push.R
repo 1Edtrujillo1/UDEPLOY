b91155c9d7d1318cd70ad089339e1cd44f5429d1
Eduardo Trujillo <act.jetv@gmail.com>
2021-03-12 10:30:28 -0600

    Corrections 2 of iterative_merge function

--- R/pull&push.R
+++ R/pull&push.R
@@ -565,7 +565,7 @@ iterative_merge <- function(dfs_list, key, keep = NULL, ...){
   }else{
     check <- clean_merged_table(df_merged = df_merged, keep = keep)
     if(is.character(check)){
-      result <- clean_merged_table(df_merged = df_merged)
+      result <- df_merged
     }else{result <- check}
   }
   result
